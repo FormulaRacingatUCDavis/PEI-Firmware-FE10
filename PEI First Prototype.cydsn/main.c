373cbad7ff50b8b18f5e7734d5fbb21c603cc59f
Justin Chang <chang.justin0106@gmail.com>
2022-11-19 12:08:06 -0800

    Updated main.c
    Updated main.c

--- PEI First Prototype.cydsn/main.c
+++ PEI First Prototype.cydsn/main.c
@@ -11,15 +11,34 @@
 */
 #include "project.h"
 
+typedef enum {
+    VEHICLE_STATE = 0x0c0,
+    SWITCHES = 0x0d0,
+    TORQUE_REQUEST_COMMAND = 0x766,
+    BMS_STATUS = 0x380,
+    PEI_CURRENT = 0x387,
+    BMS_VOLTAGES = 0x388,
+    BMS_TEMPERATURES = 0x389
+} CAN_ID;
+
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+    int state = 0;
+    
     for(;;)
     {
-        /* Place your application code here. */
+        if (state == 0) {
+        }
+        else if (state == 1) {
+        }
+        else if (state == 2) {
+        }
+        else if (state == 3) {
+        }
+            
     }
 }
 
